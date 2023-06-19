f393dbda5442cf01c0fb8d6a8d7626fabb1fc7e4
Muhammad Kausar <muhammadkausar01@gmail.com>
2023-06-19 17:12:24 +0700

    add controller laporan

--- routes/laporanRoutes.js
+++ routes/laporanRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require("../controller/index");
 
 router.get("/", controller.laporan.getAllLaporan);
+router.put("/", controller.laporan.updateLaporan);
+router.get("/:id", controller.laporan.getLaporanByid);
+router.delete("/:id", controller.laporan.deleteLaporan);
 
 module.exports = router;
